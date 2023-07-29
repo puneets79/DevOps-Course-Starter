4545beb351acbf9b11c48a7887edd33392b17f9e
puneets79 <puneet.sharma@credit-suisse.com>
2023-07-29 19:02:34 +0100

    Created index to display TODO list

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+
+from flask import Flask,render_template
 
 from todo_app.flask_config import Config
+from todo_app.data import session_items
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +10,4 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', items=session_items.get_items())
