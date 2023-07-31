0f91afd8e28e73dbf7594380c52c97b4607b034d
puneets79 <puneet.sharma@credit-suisse.com>
2023-07-31 12:52:10 +0100

    Added capability to update and add tasks

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,5 @@
 
-from flask import Flask,render_template
-
+from flask import Flask,render_template, request, redirect, url_for
 from todo_app.flask_config import Config
 from todo_app.data import session_items
 
@@ -10,4 +9,30 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return render_template('index.html', items=session_items.get_items())
+    return render_template('index.html', items=session_items.get_items(), route='display')
+
+@app.route('/add', methods=['GET','POST'])
+def addtodo():
+    if (request.method == 'GET'):
+        return render_template('index.html',route='add')
+    else:
+        taskname = request.form.get('taskname')
+        session_items.add_item(taskname)
+        return redirect('/')
+    
+@app.route('/update', methods=['GET','POST'])
+def updatetodo():
+    if (request.method == 'GET'):
+        id = request.values.get("id","")
+        item = session_items.get_item(id)
+        return render_template('index.html',route='update', item = item)
+    else:
+        id = int(request.form.get('id'))
+        taskname = request.form.get('taskname')
+        status = request.form.get('status')
+        item = {'id':id, 'title':taskname, 'status':status}
+        print (item)
+        session_items.save_item(item)
+        return redirect('/')
+
+    
